07e3a4e7d701621c192d2bf04288e386bafdcef1
Brently Bazor <brently.bazor@gmail.com>
2019-07-14 20:22:47 -0500

    finished fetch tutorial

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/components/home';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home/>
     </View>
   );
 }
